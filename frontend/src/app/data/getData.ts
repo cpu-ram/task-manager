bbde8f250c833bd8767a69d3d08bfd18d7ad9ea7
Pete <petr.stepanenko@gmail.com>
2026-01-24 19:18:13 -0600

    feat(frontend): implement data import feature
    Fixes #8.

--- frontend/src/app/data/getData.ts
+++ frontend/src/app/data/getData.ts
@@ -1,6 +1,7 @@
 import { BaseNode } from 'nested-core';
 import Domain from '../models/domain/Domain';
-import { Task } from '../models/task/Task';
+
+import { parseData } from './parseData';
 
 const seedData = new Domain({
   title: 'root',
@@ -9,37 +10,12 @@ const seedData = new Domain({
 
 function loadLocalStorageData(): BaseNode {
   const stored = localStorage.getItem('taskData');
-  return stored ? parseObject(JSON.parse(stored)) : seedData;
+  return stored ? parseData(stored) : seedData;
 }
 
 export function exportData(): string {
   const data = localStorage.getItem('taskData');
   return data ?? JSON.stringify(seedData);
-}
-
-type ParsedObjectType = {
-  type: string;
-  title: string;
-  children?: any;
-};
-
-function parseObject(object: ParsedObjectType): BaseNode {
-  let result: BaseNode | null = null;
-
-  switch (object.type) {
-    case 'domain':
-      result = new Domain(object);
-      break;
-    case 'task':
-      result = new Task(object);
-      break;
-    default:
-      throw new Error(`Unknown node type: ${object.type}`);
-  }
-  if (!result) throw new Error('Internal error.');
-
-  result.children = (object.children ?? []).map((x: ParsedObjectType) => parseObject(x));
-  return result;
 }
 
 export function getData(): BaseNode {
