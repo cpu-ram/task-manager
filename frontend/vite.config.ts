961cecde30fd29f2a59eb1927f4faf40f2676c7e
Pete <petr.stepanenko@gmail.com>
2026-01-24 18:05:01 -0600

    dev: alias nested-core to local src in package.json and tsconfig files, deduplicate react

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dotenv from 'dotenv';
+import path from 'node:path';
 
 dotenv.config();
 const allowedHostsArray = process.env.ALLOWED_HOSTS?.split(',') ?? [];
@@ -10,6 +11,13 @@ const baseLaunchDirectory = process.env.BASE_LAUNCH_DIRECTORY ?? '';
 export default defineConfig({
   base: `${baseLaunchDirectory}`,
   plugins: [react()],
+  resolve: {
+    alias: {
+      'nested-core': path.resolve(__dirname, '../../nested-core/src'),
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+  },
   build: {
     sourcemap: true,
   },
